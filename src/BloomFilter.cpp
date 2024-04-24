393034971bdc185d0baca7fc809a0d8ba47280ff
mmahdin <mahdi9roozi@gmail.com>
2024-04-25 00:02:18 +0330

    Implement add function in BloomFilter

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,1 +1,28 @@
 #include "BloomFilter.h"
+
+template <std::size_t N>
+BloomFilter<N>::BloomFilter(unsigned int num_hashes) : num_hashes{num_hashes} {
+  for (std::size_t i{}; i < 100; seeds.push_back(i), i++)
+    ;
+}
+
+template <std::size_t N>
+BloomFilter<N>::BloomFilter(const BloomFilter& other) {}
+
+template <std::size_t N>
+BloomFilter<N>::BloomFilter(BloomFilter&& other) noexcept {}
+
+template <std::size_t N>
+BloomFilter<N>::~BloomFilter() {}
+
+template <std::size_t N>
+void BloomFilter<N>::add(const std::string& item) {
+  std::size_t hashed;
+  std::size_t mod;
+  for (std::size_t i{}; i < N; i++) {
+    hashed = hash(item, seeds[i]);
+    mod = hashed % N;
+    bits.set(mod, true);
+    server->addWord(item);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
 #include <gtest/gtest.h>
 
-#include "BloomFilter.h"
-#include "Trie.h"
+#include <iostream>
 
-int main(int argc, char **argv)
-{
-    if (true) // Set to false to run unit-tests
-    {
-        // Debug section: Place your debugging code here
-    }
+// #include "BloomFilter.h"
+// #include "Trie.h"
+#include <vector>
+
+int main(int argc, char **argv) {
+  if (true)  // Set to false to run unit-tests
+  {
+    // Debug section: Place your debugging code here
+
+    std::vector<std::size_t> seeds;
+    for (std::size_t i{}; i < 100; seeds.push_back(i), i++)
+      ;
+  } else {
+    ::testing::InitGoogleTest(&argc, argv);
+    std::cout << "RUNNING TESTS ..." << std::endl;
+    int ret{RUN_ALL_TESTS()};
+    if (!ret)
+      std::cout << "<<<SUCCESS>>>" << std::endl;
     else
-    {
-        ::testing::InitGoogleTest(&argc, argv);
-        std::cout << "RUNNING TESTS ..." << std::endl;
-        int ret{RUN_ALL_TESTS()};
-        if (!ret)
-            std::cout << "<<<SUCCESS>>>" << std::endl;
-        else
-            std::cout << "FAILED" << std::endl;
-    }
-    return 0;
+      std::cout << "FAILED" << std::endl;
+  }
+  return 0;
 }
