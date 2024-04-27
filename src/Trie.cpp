7c3f7647f53592b612341647a06deaf9170cce1b
mmahdin <mahdi9roozi@gmail.com>
2024-04-27 16:38:46 +0330

    Implement Node calss

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -1,1 +1,10 @@
 #include "Trie.h"
+
+Trie::Node::Node(char data, bool is_finished)
+    : data{data}, is_finished{is_finished}, parent{nullptr} {
+  children.fill(nullptr);
+}
+
+Trie::Node::~Node() {
+  for (auto child : children) delete child;
+}
