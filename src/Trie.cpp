d9c848647cfc9163f1cfa7fbee4f71666d5b713c
mmahdin <mahdi9roozi@gmail.com>
2024-04-27 17:24:23 +0330

    Implement constructors of Trie

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -8,3 +8,24 @@ Trie::Node::Node(char data, bool is_finished)
 Trie::Node::~Node() {
   for (auto child : children) delete child;
 }
+
+Trie::Trie() : root{new Node()} {}
+
+Trie::Trie(const Trie& other) {
+  root = new Node();
+  //   *this = other;
+  root = other.root;
+}
+
+Trie::Trie(Trie&& other) {
+  root = new Node();
+  root = other.root;
+  delete other.root;
+  //   std::swap(root, other.root);
+}
+
+Trie::Trie(std::initializer_list<std::string> list) : root{new Node()} {
+  for (const auto& word : list) {
+    insert(word);
+  }
+}
