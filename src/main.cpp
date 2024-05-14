0b21ae683f044d074fb9fb876a04e426093d0bd3
mmahdin <mahdi9roozi@gmail.com>
2024-05-14 17:34:59 +0330

    Implement Message class

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 #include <iostream>
 
 #include "message.h"
-#include "server.h"
-#include "stl.h"
-#include "user.h"
+// #include "server.h"
+// #include "stl.h"
+// #include "user.h"
 
 int main(int argc, char **argv) {
   if (true)  // make false to run unit-tests
   {
     // debug section
-
+    Message msg{"salam", "man", "mahdi"};
+    std::cout << msg << std::endl;
   } else {
     ::testing::InitGoogleTest(&argc, argv);
     std::cout << "RUNNING TESTS ..." << std::endl;
--- src/message.cpp
+++ src/message.cpp
@@ -2,5 +2,29 @@
 
 Message::Message(std::string type, std::string sender, std::string receiver)
     : type{type}, sender{sender}, receiver{receiver} {
-  this->time = std::to_string(std::time(nullptr));
+  std::time_t currentTime = std::time(nullptr);
+  time = std::ctime(&currentTime);
+}
+
+Message::Message() : Message{"", "", ""} {}
+
+std::string Message::get_type() const { return type; }
+
+std::string Message::get_sender() const { return sender; }
+
+std::string Message::get_receiver() const { return receiver; }
+
+std::string Message::get_time() const { return time; }
+
+void Message::print(std::ostream& os) const {
+  os << "*************************\n"
+     << sender << "->" << receiver << "\n"
+     << "message type: " << type << "\n"
+     << "message time:" << time << "\n"
+     << "*************************" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Message& msg) {
+  msg.print(os);
+  return os;
 }
