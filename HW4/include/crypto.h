a989d48d53403ad904070471d0c17697dc3a80a9
mmahdin <mahdi9roozi@gmail.com>
2024-06-08 13:54:02 +0330

    Update files

--- HW4/include/crypto.h
+++ HW4/include/crypto.h
@@ -20,23 +20,23 @@ RSA* createPrivateRSA(std::string key);
 RSA* createPublicRSA(std::string key);
 
 bool RSASign(RSA* rsa, const unsigned char* Msg, size_t MsgLen,
-             unsigned char** EncMsg, size_t* MsgLenEnc);
+			 unsigned char** EncMsg, size_t* MsgLenEnc);
 
 bool RSAVerifySignature(RSA* rsa, unsigned char* MsgHash, size_t MsgHashLen,
-                        const char* Msg, size_t MsgLen, bool* Authentic);
+						const char* Msg, size_t MsgLen, bool* Authentic);
 
 void Base64Encode(const unsigned char* buffer, size_t length,
-                  char** base64Text);
+				  char** base64Text);
 
 size_t calcDecodeLength(const char* b64input);
 
 void Base64Decode(const char* b64message, unsigned char** buffer,
-                  size_t* length);
+				  size_t* length);
 
 std::string signMessage(std::string privateKey, std::string plainText);
 
 bool verifySignature(std::string publicKey, std::string plainText,
-                     std::string signatureBase64);
+					 std::string signatureBase64);
 
 const char* keyFromRSA(RSA* rsa, bool isPrivate);
 
@@ -49,4 +49,4 @@ std::string sha256_tmp(std::string s);
 std::string sha256(std::string s);
 
 }  // namespace crypto
-#endif  // CRYPTO_H
+#endif	// CRYPTO_H
--- HW4/src/main.cpp
+++ HW4/src/main.cpp
@@ -98,7 +98,7 @@ void test_copy_if_even() {
 }
 
 int main(int argc, char** argv) {
-  if (true)  // make false to run unit-tests
+  if (false)  // make false to run unit-tests
   {
     // debug section
     // Message testMessage("text", "Alice", "Bob");
